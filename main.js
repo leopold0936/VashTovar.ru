4c3c0908d26b09f73b0827502c122dc44d23b29e
Leo <leo.craft@icloud.com>
2024-10-04 12:18:46 +0300

    1 commit

---
+++ main.js
@@ -0,0 +1,16 @@
+"use strict";
+
+console.log("hello world");
+
+// let name = prompt("Какой нэйм у тебя ээ?");
+// alert("Хорошо, " + name + ", иди ...");
+// alert("... и решай матешу :)");
+// let numX = Number(prompt("Введи число X:"));
+// let numY = +prompt("Введи число Y:");
+// let numZ = parseInt(prompt("Введи число Z:"));
+// let otvet = numX + numY + numZ;
+// alert("Ответ: " + otvet);
+
+// let age = +prompt("Какой ты старый?");
+// let old = 2024 - age;
+// alert("Ты родился в " + old + " году. Ну ты и старый");
